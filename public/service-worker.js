8079fd1b51165e3fd89baf1651a37d9094c62d84
Patrick Mahloy <PatrickMahloy@Patricks-MBP.fios-router.home>
2020-10-20 21:08:28 -0400

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,11 @@
 const FILES_TO_CACHE = [
   '/',
-  '/index.html',
-  '/favorites.html',
-  '/topic.html',
-  '/assets/css/style.css',
-  '/dist/app.bundle.js',
-  '/dist/favorites.bundle.js',
-  '/dist/topic.bundle.js',
-  'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  '/db.js',
+  '/index.js',
+  '/manifest.webmanifest',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 const PRECACHE = 'precache-v1';
@@ -20,26 +17,6 @@ self.addEventListener('install', (event) => {
       .open(PRECACHE)
       .then((cache) => cache.addAll(FILES_TO_CACHE))
       .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
   );
 });
 
